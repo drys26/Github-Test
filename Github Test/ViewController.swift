dbe17e88e6459f7df82e8be215f52ed0dd8907e8
jerixkiler <jerixkiller3@gmail.com>
2017-05-30 11:03:02 +0800

    aw

--- Github Test/ViewController.swift
+++ Github Test/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextPage(_ sender: Any) {
+        performSegue(withIdentifier: "aws", sender: nil)
+    }
 
 }
 
